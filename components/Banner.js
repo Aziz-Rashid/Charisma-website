c9e1bc10da7e5e0c7d7c4c305f522c7badc75f45
Aziz-Rashid <axixraxhid@gmail.com>
2021-05-31 02:19:25 +0500

    update

--- components/Banner.js
+++ components/Banner.js
@@ -16,14 +16,31 @@ const Banner = ({ newData }) => {
                     </div>
                 ))
             ))}
-            <div className="banners_box">
-                <h1 className="banners_heading">Relationships <span className="masive">MASSIVELY</span> impact the quality of your life</h1>
-                <h4 className="banners_subHeading">Relationships control your:</h4>
-                <p className="bulletpoint">success in business</p>
-                <p className="bulletpoint">success with dating</p>
-                <p className="bulletpoint">ability to materialize a larger vision</p>
-                <p className="bullet_para">…success in these all comes down to the quality of the relationships in your life, and your ability to create amazing new ones on command.</p>
+            {newData.map(el => (
+                el.bannerBox.map(data => (
+                    <div className="banners_box">
+                        <h1 className="banners_heading">{data.Relationships} <span className="masive">{data.masive}</span> {data.Relationships2}</h1>
+                        <h4 className="banners_subHeading">{data.name}</h4>
+                        <p className="bulletpoint">{data.bullet1}</p>
+                        <p className="bulletpoint">{data.bullet2}</p>
+                        <p className="bulletpoint">{data.bullet3}</p>
+                        <p className="bullet_para">{data.para}</p>
+                    </div>
+                ))
+            ))}
+            <div className="containers_block">
+                <div className="containers_block2">
+                <h2 className="conatiener">I want to show you how to create the relationships, friendships, and life you’ve always dreamed of.</h2>
+                <div className="conatiners_box">
+                    <h4 className="parag">Let me paint you a picture:</h4>
+                    <p className="parag2">You’re that person with their chest up, head held high.</p>
+                    <p className="parag3">You easily speak through a smile… you always seem to know exactly what to say.</p>
+                    <p className="parag4">You have the confidence to walk up and introduce yourself or strike up a conversation with anyone… and you leave them thinking:</p>
+                    <p className="parag5">"Wow... I need to see them again"</p>
+                </div>
+                </div>
             </div>
+
         </div>
     )
 }
